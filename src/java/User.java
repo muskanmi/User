e6386d8de7973a5f19af76d2a3072609cd96220b
Dell <muskan.mittal_cs18@gla.ac.in>
2019-09-28 14:17:47 +0530

    first Git commit Java Project

---
+++ src/java/User.java
@@ -0,0 +1,9 @@
+package java;
+
+public class User {
+    public static void main(String[] args) {
+        String s1 = "StringHandling";
+        char ch = s1.charAt(6);
+        System.out.println(ch);
+    }
+}
